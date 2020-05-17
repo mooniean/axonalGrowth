d00f59ad861e3c212a10140151cff012c292c4a1
Mauricio Moreira <phydev@users.noreply.github.com>
2020-05-17 01:17:57 +0100

    test file

---
+++ tests/general.py
@@ -0,0 +1,17 @@
+import unittest
+import pyaxon.functions
+
+
+class TestGeneral(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        np.random.RandomState(0)
+
+    def test_init(self):
+        # tests should be written here
+        self.assertEqual(h(1.),h(1.))
+    
+
+if __name__ == '__main__':
+    unittest.main()
